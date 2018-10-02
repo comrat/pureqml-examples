76a612d9757f416f664066e47ea8d93b78a3cea0
Alexey Pobery <poberyaa@gmail.com>
2018-10-03 00:07:44 +0300

    Add plugin for voice recognition

--- dist.platform.android/build.py
+++ dist.platform.android/build.py
@@ -2,4 +2,5 @@
 {% block plugins %}
 	{{ super() }}
         os.system('cordova plugins add https://github.com/cordova-plugin-camera-preview/cordova-plugin-camera-preview.git')
+        os.system('cordova plugins add cordova-plugin-speechrecognition')
 {% endblock %}
